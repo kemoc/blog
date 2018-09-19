bb814990fab4497e54f9a36564413eb2d3118b97
RadekZ <radoslaw.zadroga@db-team.pl>
2018-09-19 16:50:02 +0200

    project: start

---
+++ src/Symfony/Component/Translation/IdentityTranslator.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\Translation;
+
+use Symfony\Contracts\Translation\TranslatorTrait;
+
+/**
+ * IdentityTranslator does not translate anything.
+ *
+ * @author Fabien Potencier <fabien@symfony.com>
+ */
+class IdentityTranslator implements TranslatorInterface
+{
+    use TranslatorTrait {
+        transChoice as private doTransChoice;
+    }
+
+    private $selector;
+
+    /**
+     * @param MessageSelector|null $selector The message selector for pluralization
+     */
+    public function __construct(MessageSelector $selector = null)
+    {
+        $this->selector = $selector;
+
+        if (\get_class($this) !== __CLASS__) {
+            @trigger_error(sprintf('Calling "%s()" is deprecated since Symfony 4.2.'), E_USER_DEPRECATED);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function transChoice($id, $number, array $parameters = array(), $domain = null, $locale = null)
+    {
+        if ($this->selector) {
+            return strtr($this->selector->choose((string) $id, (int) $number, $locale ?: $this->getLocale()), $parameters);
+        }
+
+        return $this->doTransChoice($id, $number, $parameters, $domain, $locale);
+    }
+
+    private function getPluralizationRule(int $number, string $locale): int
+    {
+        return PluralizationRules::get($number, $locale, false);
+    }
+}
